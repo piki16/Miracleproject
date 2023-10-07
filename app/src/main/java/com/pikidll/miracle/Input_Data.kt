28b8075180b418d796c519a6082fcb145456b7e4
user <vickyaulia600.ac.id>
2023-10-07 08:27:48 +0700

    Initial commit

---
+++ app/src/main/java/com/pikidll/miracle/Input_Data.kt
@@ -0,0 +1,11 @@
+package com.pikidll.miracle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Input_Data : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_data)
+    }
+}
